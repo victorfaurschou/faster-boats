d3b6c5a04140fadd9f69ea62c1911edb1d06a96d
Victor Faurschou <mail@victorfaurschou.com>
2026-06-14 09:21:07 +0200

    Fix rotation speed and bump reverse to 1.3x

--- src/main/java/com/victorfaurschou/fasterboats/mixin/BoatSpeedMixin.java
+++ src/main/java/com/victorfaurschou/fasterboats/mixin/BoatSpeedMixin.java
@@ -9,17 +9,11 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class BoatSpeedMixin {
     @ModifyConstant(method = "controlBoat", constant = @Constant(floatValue = 0.04F))
     private float boostForwardAcceleration(float original) {
-        return 0.03F;
+        return 0.06F;
     }
 
     @ModifyConstant(method = "controlBoat", constant = @Constant(floatValue = 0.005F, ordinal = 1))
     private float boostReverseAcceleration(float original) {
-        return 0.003F;
-    }
-
-    // Reduce water drag to maintain 1.5x top speed with the lower acceleration (ordinal 0 = IN_WATER case)
-    @ModifyConstant(method = "floatBoat", constant = @Constant(floatValue = 0.9F, ordinal = 0))
-    private float reduceWaterDrag(float original) {
-        return 0.95F;
+        return 0.0065F;
     }
 }
