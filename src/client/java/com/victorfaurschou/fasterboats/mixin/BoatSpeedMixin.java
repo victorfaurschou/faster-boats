e814996d395c6a0578e488b663604851a499d274
Victor Faurschou <mail@victorfaurschou.com>
2026-07-08 13:57:29 +0200

    Backport to 1.21.2-1.21.10 (pre-boat-subpackage-move)

--- src/client/java/com/victorfaurschou/fasterboats/mixin/BoatSpeedMixin.java
+++ src/client/java/com/victorfaurschou/fasterboats/mixin/BoatSpeedMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.vehicle.boat.AbstractBoat;
+import net.minecraft.world.entity.vehicle.AbstractBoat;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
